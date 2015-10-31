6024e809351c08551d3b7b09a2edeedda847a4a1
Tianyu Chen <tianyuc@andrew.cmu.edu>
2015-10-30 22:58:03 -0400

    storagetest 6/6, libtest 19/19, tribtest 24/29

--- src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
+++ src/github.com/cmu440/tribbler/storageserver/storageserver_impl.go
@@ -2,19 +2,26 @@ package storageserver
 
 import (
 	// "container/list"
-	"errors"
+
 	"fmt"
 	"net"
 	"net/http"
 	"net/rpc"
 	"strconv"
+	"time"
 
 	"github.com/cmu440/tribbler/rpc/storagerpc"
 )
 
 type storageServer struct {
-	ItemMap map[string]string   // PostKey to value
-	ListMap map[string][]string // key defined in util
+	ItemMap  map[string]string   // PostKey to value
+	ListMap  map[string][]string // key defined in util
+	numNodes int                 // number of storage servers expected
+	// for slave servers
+	Ready chan bool
+
+	// for master servers
+	ServerNodes []storagerpc.Node
 }
 
 // NewStorageServer creates and starts a new StorageServer. masterServerHostPort
@@ -30,36 +37,95 @@ func NewStorageServer(masterServerHostPort string, numNodes, port int, nodeID ui
 	storageServer := new(storageServer)
 	storageServer.ItemMap = make(map[string]string)
 	storageServer.ListMap = make(map[string][]string)
+	storageServer.numNodes = numNodes
 
 	hostPort := net.JoinHostPort("localhost", strconv.Itoa(port))
-	// for checkpoint, ignore masterServerHostPort first, but need to implement in the future
-	// Create the server socket that will listen for incoming RPCs.
 	listener, err := net.Listen("tcp", hostPort)
 	if err != nil {
 		fmt.Println("Error on StorageServer Listen", err)
 		return nil, err
 	}
 
-	// Wrap the Server before registering it for RPC.
+	if masterServerHostPort == "" { // master server
+		storageServer.ServerNodes = make([]storagerpc.Node, 0)
+		// register itself
+		node := &storagerpc.Node{NodeID: nodeID, HostPort: hostPort}
+		storageServer.ServerNodes = append(storageServer.ServerNodes, *node)
+		// Wrap the Server before registering it for RPC.
+		if err = rpc.RegisterName("StorageServer", storagerpc.Wrap(storageServer)); err != nil {
+			fmt.Println("Error on StorageServer RegisterName", err)
+			return nil, err
+		}
+		rpc.HandleHTTP()
+		go http.Serve(listener, nil)
+		return storageServer, nil
+	}
+
+	// slave server
+	storageServer.Ready = make(chan bool)
+	slave, err := rpc.DialHTTP("tcp", masterServerHostPort)
+	if err != nil {
+		fmt.Println("Error: NewLibstore DialHTTP:", err)
+		return nil, err
+	}
+	go func() {
+		node := &storagerpc.Node{HostPort: hostPort, NodeID: nodeID}
+		args := &storagerpc.RegisterArgs{ServerInfo: *node}
+		var reply storagerpc.RegisterReply
+		for {
+			_ = slave.Call("StorageServer.RegisterServer", args, &reply)
+			if reply.Status != storagerpc.OK {
+				time.Sleep(1 * time.Second)
+			} else { // Status = OK
+				storageServer.ServerNodes = reply.Servers
+				storageServer.Ready <- true
+				return
+			}
+		}
+	}()
+	// block slave servers
+	_ = <-storageServer.Ready
 	if err = rpc.RegisterName("StorageServer", storagerpc.Wrap(storageServer)); err != nil {
 		fmt.Println("Error on StorageServer RegisterName", err)
 		return nil, err
 	}
-
 	// Setup the HTTP handler that will server incoming RPCs and
 	// serve requests in a background goroutine.
 	rpc.HandleHTTP()
 	go http.Serve(listener, nil)
-
 	return storageServer, nil
 }
 
 func (ss *storageServer) RegisterServer(args *storagerpc.RegisterArgs, reply *storagerpc.RegisterReply) error {
-	return errors.New("not implemented")
+	found := false
+	for _, ServerNode := range ss.ServerNodes {
+		if ServerNode == args.ServerInfo {
+			found = true
+			break
+		}
+	}
+	if !found { // new register request
+		node := &storagerpc.Node{NodeID: args.ServerInfo.NodeID, HostPort: args.ServerInfo.HostPort}
+		ss.ServerNodes = append(ss.ServerNodes, *node)
+	}
+	if len(ss.ServerNodes) == ss.numNodes { // all servers available
+		reply.Status = storagerpc.OK
+		reply.Servers = ss.ServerNodes
+		return nil
+	}
+	reply.Status = storagerpc.NotReady
+	return nil
 }
 
 func (ss *storageServer) GetServers(args *storagerpc.GetServersArgs, reply *storagerpc.GetServersReply) error {
-	return errors.New("not implemented")
+	if len(ss.ServerNodes) == ss.numNodes { // all servers available
+		reply.Status = storagerpc.OK
+		reply.Servers = ss.ServerNodes
+		return nil
+	}
+	reply.Status = storagerpc.NotReady
+	reply.Servers = ss.ServerNodes
+	return nil
 }
 
 func (ss *storageServer) Get(args *storagerpc.GetArgs, reply *storagerpc.GetReply) error {
