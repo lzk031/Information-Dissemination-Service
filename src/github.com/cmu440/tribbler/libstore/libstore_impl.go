82dc187008349b7192fc122cf640410dabc97128
Tianyu Chen <tianyuc@andrew.cmu.edu>
2015-11-01 10:33:29 -0500

    draft version libstore, 30/41

--- src/github.com/cmu440/tribbler/libstore/libstore_impl.go
+++ src/github.com/cmu440/tribbler/libstore/libstore_impl.go
@@ -7,8 +7,18 @@ import (
 	"sort"
 	"time"
 
+	"github.com/cmu440/tribbler/rpc/librpc"
 	"github.com/cmu440/tribbler/rpc/storagerpc"
 	// "github.com/cmu440/tribbler/util"
+)
+
+type CacheStatus int
+
+// cache constants
+const (
+	Found     = iota + 1 // item found in cache
+	WantLease            // item not in cache and want lease
+	SendQuery            // item not in cache but does not want lease
 )
 
 type libstore struct {
@@ -16,7 +26,29 @@ type libstore struct {
 	ServerNodes []storagerpc.Node
 	MyHostPort  string
 
-	Ready chan storagerpc.GetServersReply
+	// critical section
+	Cache     map[string]cacheitem
+	KeyRecord map[string][]time.Time
+
+	Ready           chan storagerpc.GetServersReply
+	checkCache      chan string
+	checkCacheReply chan cacheitem
+	revokeCache     chan string
+	// revokeCacheReply chan bool
+	// expireCache      chan string
+	addCache chan addcachePack
+	// addCacheReply chan bool
+	CacheReply chan bool
+}
+
+type addcachePack struct {
+	Key  string
+	Item cacheitem
+}
+
+type cacheitem struct {
+	Status CacheStatus
+	Item   interface{}
 }
 
 type NodeSlice []storagerpc.Node
@@ -59,10 +91,19 @@ func (key NodeSlice) Less(i, j int) bool {
 // need to create a brand new HTTP handler to serve the requests (the Libstore may
 // simply reuse the TribServer's HTTP handler since the two run in the same process).
 func NewLibstore(masterServerHostPort, myHostPort string, mode LeaseMode) (Libstore, error) {
-	Libstore := new(libstore)
-	Libstore.lib = make(map[string]*rpc.Client)
-	Libstore.MyHostPort = myHostPort
-	Libstore.Ready = make(chan storagerpc.GetServersReply)
+	ls := new(libstore)
+	ls.MyHostPort = myHostPort
+	ls.lib = make(map[string]*rpc.Client)
+	ls.Cache = make(map[string]cacheitem)
+	ls.KeyRecord = make(map[string][]time.Time)
+
+	ls.Ready = make(chan storagerpc.GetServersReply)
+	ls.checkCache = make(chan string)
+	ls.checkCacheReply = make(chan cacheitem)
+	ls.revokeCache = make(chan string)
+	// ls.revokeCacheReply = make(chan bool)
+	ls.addCache = make(chan addcachePack)
+	ls.CacheReply = make(chan bool)
 
 	// listen to master server
 	lib, err := rpc.DialHTTP("tcp", masterServerHostPort)
@@ -70,24 +111,104 @@ func NewLibstore(masterServerHostPort, myHostPort string, mode LeaseMode) (Libst
 		fmt.Println("Error: NewLibstore DialHTTP:", err)
 		return nil, err
 	}
-	Libstore.lib[masterServerHostPort] = lib
-	go Libstore.ContactMasterServer(masterServerHostPort)
+	ls.lib[masterServerHostPort] = lib
+	go ls.ContactMasterServer(masterServerHostPort)
 
 	// block slave servers
-	reply := <-Libstore.Ready
+	reply := <-ls.Ready
 	if reply.Status != storagerpc.OK { // fail retry upto 5 times
 		return nil, errors.New("Error: Master Storage Server is not ready")
 	}
 	// sort reply by NodeID
 	Servers := NodeSlice(reply.Servers)
 	sort.Sort(Servers)
-	Libstore.ServerNodes = []storagerpc.Node(Servers)
+	ls.ServerNodes = []storagerpc.Node(Servers)
 	// create rpc to all storage servers
-	if aerr := Libstore.AddStorageRPC(); aerr != nil {
+	if aerr := ls.AddStorageRPC(); aerr != nil {
 		return nil, aerr
 	}
-	// rpc.RegisterName("LeaseCallbacks", librpc.Wrap(libstore))
-	return Libstore, nil
+	go ls.CacheHandler()
+
+	// register rpc to receive call backs from storage servers
+	// listener, err := net.Listen("tcp", myHostPort)
+	// if err != nil {
+	// 	fmt.Println("Error on LibServer Listen", err)
+	// 	return nil, err
+	// }
+	if rerr := rpc.RegisterName("LeaseCallbacks", librpc.Wrap(ls)); rerr != nil {
+		fmt.Println("Error on LibServer RegisterName", err)
+		return nil, err
+	}
+	// rpc.HandleHTTP()
+	// go http.Serve(listener, nil)
+
+	return ls, nil
+}
+
+func (ls *libstore) CacheHandler() {
+	for {
+		select {
+		case key := <-ls.checkCache:
+			CacheItem := ls.CheckCache(key)
+			ls.checkCacheReply <- CacheItem
+		case key := <-ls.revokeCache:
+			delete(ls.Cache, key)
+			ls.CacheReply <- true
+		case pack := <-ls.addCache:
+			ls.Cache[pack.Key] = pack.Item
+			ls.CacheReply <- true
+		}
+	}
+}
+
+func (ls *libstore) CheckCache(key string) cacheitem {
+	item, found := ls.Cache[key]
+	if found { // item already in cache
+		return item
+	}
+	status := ls.CheckRecord(key)
+	ItemReturn := &cacheitem{Status: status}
+	// ItemReturn = &cacheitem{Status: status, Item: item}
+	return *ItemReturn
+}
+
+func (ls *libstore) CheckRecord(key string) CacheStatus {
+	record, found := ls.KeyRecord[key]
+	if !found {
+		record = make([]time.Time, 0)
+	}
+	if len(record) < storagerpc.QueryCacheThresh {
+		record = append(record, time.Now())
+		ls.KeyRecord[key] = record // update record
+		return SendQuery
+	}
+	// check timestamp
+	timestamp := record[len(record)-storagerpc.QueryCacheThresh]
+	record = append(record, time.Now())
+	ls.KeyRecord[key] = record[(len(record) - storagerpc.QueryCacheThresh):] // update record
+	duration := -timestamp.Sub(time.Now())
+	if duration < storagerpc.QueryCacheSeconds { // want lease
+		return WantLease
+	}
+	return SendQuery
+}
+
+func (ls *libstore) ContactMasterServer(masterServerHostPort string) {
+	lib := ls.lib[masterServerHostPort]
+	args := &storagerpc.GetServersArgs{}
+	var reply storagerpc.GetServersReply
+	for i := 0; i < 6; i++ { // retry upto 5 times
+		_ = lib.Call("StorageServer.GetServers", args, &reply)
+		if reply.Status != storagerpc.OK {
+			time.Sleep(1 * time.Second)
+		} else { // Status = OK
+			fmt.Println(reply.Servers)
+			ls.Ready <- reply
+			return
+		}
+	}
+	ls.Ready <- reply
+	return
 }
 
 func (ls *libstore) AddStorageRPC() error {
@@ -103,40 +224,9 @@ func (ls *libstore) AddStorageRPC() error {
 	return nil
 }
 
-func (ls *libstore) ContactMasterServer(masterServerHostPort string) {
-	lib := ls.lib[masterServerHostPort]
-	args := &storagerpc.GetServersArgs{}
-	var reply storagerpc.GetServersReply
-	for i := 0; i < 6; i++ {
-		_ = lib.Call("StorageServer.GetServers", args, &reply)
-		if reply.Status != storagerpc.OK {
-			time.Sleep(1 * time.Second)
-		} else { // Status = OK
-			fmt.Println(reply.Servers)
-			ls.Ready <- reply
-			return
-		}
-	}
-	ls.Ready <- reply
-	return
-}
-
-func (ls *libstore) Get(key string) (string, error) {
-	// fmt.Println("Lib: Get")
-	// defer fmt.Println("Lib: Get Done")
-	lib := ls.FindRPC(key)
-	args := &storagerpc.GetArgs{Key: key, WantLease: false, HostPort: ls.MyHostPort}
-	var reply storagerpc.GetReply
-	_ = lib.Call("StorageServer.Get", args, &reply)
-	if reply.Status != storagerpc.OK {
-		return "", errors.New("Error on lib:Get")
-	}
-	return reply.Value, nil
-}
-
 func (ls *libstore) FindRPC(key string) *rpc.Client {
 	NodeNum := StoreHash(key)
-	fmt.Println(NodeNum)
+	// fmt.Println(NodeNum)
 	for _, ServerNode := range ls.ServerNodes {
 		if ServerNode.NodeID >= NodeNum {
 			return ls.lib[ServerNode.HostPort]
@@ -144,6 +234,35 @@ func (ls *libstore) FindRPC(key string) *rpc.Client {
 	}
 	// if cannot find, choose the first node
 	return ls.lib[ls.ServerNodes[0].HostPort]
+}
+
+func (ls *libstore) Get(key string) (string, error) {
+	// fmt.Println("Lib: Get")
+	// defer fmt.Println("Lib: Get Done")
+	wantlease := false
+	ls.checkCache <- key
+	item := <-ls.checkCacheReply
+	switch item.Status {
+	case Found:
+		return item.Item.(string), nil
+	case WantLease:
+		wantlease = true
+	}
+	lib := ls.FindRPC(key)
+	args := &storagerpc.GetArgs{Key: key, WantLease: wantlease, HostPort: ls.MyHostPort}
+	var reply storagerpc.GetReply
+	_ = lib.Call("StorageServer.Get", args, &reply)
+	if reply.Status != storagerpc.OK {
+		return "", errors.New("Error on lib:Get")
+	}
+	if reply.Lease.Granted {
+		item := &cacheitem{Status: Found, Item: reply.Value}
+		pack := &addcachePack{Key: key, Item: *item}
+		ls.addCache <- *pack
+		<-ls.CacheReply
+		go ls.ExpireCache(reply.Lease, key)
+	}
+	return reply.Value, nil
 }
 
 func (ls *libstore) Put(key, value string) error {
@@ -175,12 +294,28 @@ func (ls *libstore) Delete(key string) error {
 func (ls *libstore) GetList(key string) ([]string, error) {
 	// fmt.Println("Lib: GetList")
 	// defer fmt.Println("Lib: GetList Done")
+	wantlease := false
+	ls.checkCache <- key
+	item := <-ls.checkCacheReply
+	switch item.Status {
+	case Found:
+		return item.Item.([]string), nil
+	case WantLease:
+		wantlease = true
+	}
 	lib := ls.FindRPC(key)
-	args := &storagerpc.GetArgs{Key: key, WantLease: false, HostPort: ls.MyHostPort}
+	args := &storagerpc.GetArgs{Key: key, WantLease: wantlease, HostPort: ls.MyHostPort}
 	var reply storagerpc.GetListReply
 	_ = lib.Call("StorageServer.GetList", args, &reply)
 	if reply.Status != storagerpc.OK {
 		return nil, errors.New("Error on lib:GetList")
+	}
+	if reply.Lease.Granted {
+		item := &cacheitem{Status: Found, Item: reply.Value}
+		pack := &addcachePack{Key: key, Item: *item}
+		ls.addCache <- *pack
+		<-ls.CacheReply
+		go ls.ExpireCache(reply.Lease, key)
 	}
 	return reply.Value, nil
 }
@@ -212,5 +347,14 @@ func (ls *libstore) AppendToList(key, newItem string) error {
 }
 
 func (ls *libstore) RevokeLease(args *storagerpc.RevokeLeaseArgs, reply *storagerpc.RevokeLeaseReply) error {
-	return errors.New("not implemented")
+	ls.revokeCache <- args.Key
+	<-ls.CacheReply
+	reply.Status = storagerpc.OK
+	return nil
+}
+
+func (ls *libstore) ExpireCache(lease storagerpc.Lease, key string) {
+	<-time.After(time.Duration(lease.ValidSeconds) * time.Second)
+	ls.revokeCache <- key
+	<-ls.CacheReply
 }
